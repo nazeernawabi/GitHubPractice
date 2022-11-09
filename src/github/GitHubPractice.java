0643f466493583b0f75e8af4e3d663fc8f52e141
Nazeer Nawabi <nazeernawabi@yahoo.com>
2022-11-08 21:54:18 -0800

    test2

--- src/github/GitHubPractice.java
+++ src/github/GitHubPractice.java
@@ -4,7 +4,16 @@ public class GitHubPractice {
 
 	public static void main(String[] args) {
 		
+
 		System.out.println("GitHub practice is fun");
+		System.out.println("GitHub practice is fun");
+		System.out.println("GitHub practice is fun");
+		System.out.println("GitHub practice is fun");
+		System.out.println("GitHub practice is fun");
+		System.out.println("GitHub practice is fun");
+		System.out.println("GitHub practice is fun");
+		
+		
 
 	}
 
